512de93c8904ac4f755e81efa0e13086c46addf3
Irzna <ir.zakharyan@gmail.com>
2022-09-06 13:41:21 +0400

    with css

--- script.js
+++ script.js
@@ -1,80 +1,79 @@
-// function createNode(element) {
-//   return document.createElement(element);
-// }
+function deleteChildNode(parent) {
+  if (parent.hasChildNodes()) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
+}
+function hidenEl(parent) {
+  return (parent.hidden = true);
+}
+function showEl(parent) {
+  return (parent.hidden = false);
+}
 
-// function append(parent, el) {
-//   return parent.appendChild(el);
-// }
-
-// //const ul = document.getElementById('fetchinfo');
-// const url_api = '"https://jsonplaceholder.typicode.com/posts';
-
-// const getButton = document.getElementById("button");
-
-// const getPosts = () => {
-//   //const titlefetch = await fetch("https://jsonplaceholder.typicode.com/posts")
-//   const titlefetch = fetch(url_api)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       data.forEach(function (el) {
-//         const newLinode = createNode("li"); //document.createElement("li");
-//         const newTitleiNode = createNode("div"); //document.createElement("div");
-//         const newBodynode = createNode("div"); // document.createElement("div");
-
-//         newTitleiNode.innerHTML = el.title;
-//         newBodynode.innerHTML = el.body;
-//         //append()
-//         newLinode.appendChild(newTitleiNode);
-//         newLinode.appendChild(newBodynode);
-//         post.appendChild(newLinode);
-//         //new
-//       });
-//     })
-//     .catch((er) => console.log(er));
-//   // titlefetch.forEach((el) => {});
-//   return titlefetch;
-// };
-// getButton.addEventListener("click", getPosts());
-
-//sozdat button ,najimaya na nego  doljen poyavitsya picture pokazivauaschii looading, posletogo ka zagruzitsya finally ubiraem loading, i dannie doljni pokazatsya  v blokax , vnizu doljno bit mesto 1 i knopka ,najimaya na knopku poyavlyaetsya tolko s etim id
-//mushegh
 const API_URL = `https://jsonplaceholder.typicode.com/posts`;
-
+console.log(API_URL);
 const postsNode = document.getElementById("posts");
 const postsGetButtonNode = document.getElementById("postsGetButton");
 const filterInputNode = document.getElementById("filter-input");
+const filterSubmit = document.getElementById("filter-text");
+const loadShow = document.getElementById("load");
 
 let store;
 
 const getPosts = () => {
+  deleteChildNode(postsNode);
   fetch(API_URL)
     .then((response) => response.json())
     .then((data) => {
       store = data;
-      data.forEach((el) => {
-        const newLiNode = document.createElement("li");
-        const newTitleNode = document.createElement("div");
-        const newBodyNode = document.createElement("div");
+      showEl(loadShow);
+      setTimeout(function () {
+        rendering(store);
+        hidenEl(loadShow);
+      }, 1000);
 
-        newTitleNode.innerHTML = el.title;
-        newBodyNode.innerHTML = el.body;
-        newLiNode.appendChild(newTitleNode);
-        newLiNode.appendChild(newBodyNode);
-
-        postsNode.appendChild(newLiNode);
-      });
+      //delay(50000, rendering,store);
     });
 };
 
 function filterPosts(userId, posts) {
-  console.log(posts);
+  console.log(typeof userId);
+  if (userId === 0) {
+    return posts;
+  }
   return posts.filter((post) => post.userId === userId);
 }
-
-filterInputNode.addEventListener("change", () => {
+function getFiltredPost() {
   const value = filterInputNode.value;
-  console.log(value);
+  //console.log(value);
   const filteredPosts = filterPosts(+value, store);
-  console.log(filteredPosts);
-});
+
+  deleteChildNode(postsNode);
+  showEl(loadShow);
+  setTimeout(function () {
+    rendering(filteredPosts);
+    hidenEl(loadShow);
+  }, 1000);
+}
+
+function rendering(data) {
+  data.forEach((el) => {
+    const newLiNode = document.createElement("li");
+    const newTitleNode = document.createElement("div");
+    const newBodyNode = document.createElement("div");
+
+    newTitleNode.innerHTML = el.title;
+    newBodyNode.innerHTML = el.body;
+    newLiNode.appendChild(newTitleNode);
+    newLiNode.appendChild(newBodyNode);
+
+    postsNode.appendChild(newLiNode);
+  });
+}
+
 postsGetButtonNode.addEventListener("click", getPosts);
+
+//filterInputNode.addEventListener("change", getFiltredPost);
+filterSubmit.addEventListener("click", getFiltredPost);
